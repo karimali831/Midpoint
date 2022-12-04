1851b85c3a8fb0b099fdffa1f005f85a61fcd1fe
Karim Ali <karimali831@googlemail.com>
2022-12-04 21:30:17 +0000

    start DJ interface

--- Client/Host/packages/shared/src/components/Dashboard/Stream/Setup.tsx
+++ Client/Host/packages/shared/src/components/Dashboard/Stream/Setup.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { getMidiState } from '../../../state/contexts/midi/Selectors';
 
 export const StreamSetup = () => {
     React.useEffect(() => {}, []);
 
+    const { activeInput, inputs, controllerReady } = useSelector(getMidiState);
+
     return (
         <div>
-            <span>Setup</span>
+            <span>Set Controller</span>
+            <div
+                style={{
+                    borderRadius: 10,
+                    padding: 10,
+                    background: '#253856',
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}
+            >
+                <span>Pioneer DDJ-400</span>
+                <span>Pioneer DDJ-400</span>
+                <span>Pioneer DDJ-400</span>
+                <span>Pioneer DDJ-400</span>
+
+                {inputs
+                    .filter(
+                        (x) =>
+                            x.connection === 'open' && x.state === 'connected'
+                    )
+                    .map((input) => (
+                        <span>{input.name}</span>
+                    ))}
+            </div>
         </div>
     );
 };
